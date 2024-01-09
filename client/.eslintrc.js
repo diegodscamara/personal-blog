1a2d414072258bff9fc1597adb16262c2072b5b4
Diego Câmara <dcamara@genetec.com>
2024-01-08 21:21:43 -0300

    project setufrom 'react

---
+++ client/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      extends: [
+        'plugin:@typescript-eslint/recommended', // Ensures TypeScript support
+      ],
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'], // Include TypeScript ESLint plugin
+  rules: {
+    // Define your custom rules or override defaults here
+  },
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
